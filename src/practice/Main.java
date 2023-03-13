414ca77459fad70aa0b3512c5169bd257e309566
Olena Zaviriukha <olenazaviriukha@gmail.com>
2023-03-13 15:26:08 +0200

    Initial commit

---
+++ src/practice/Main.java
@@ -0,0 +1,39 @@
+package practice;
+
+//Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique
+// element appears only once. The relative order of the elements should be kept the same.
+//
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed
+// in the first part of the array nums. More formally, if there are k elements after removing the duplicates,
+// then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first
+// k elements.
+//
+// Return k after placing the final result in the first k slots of nums.
+//
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1)
+// extra memory.
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        removeDuplicates(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+
+        int writePosition = 1;
+        for (int index = 0; index < nums.length - 1; index++) {
+            if (nums[index] < nums[index + 1]) {
+                nums[writePosition] = nums[index +1];
+                writePosition++;
+
+            }
+        }
+        return writePosition;
+
+    }
+}
